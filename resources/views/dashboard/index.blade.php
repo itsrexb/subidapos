31182dfef83f5bd05a91a392ff806eab16161f82
Rex Bengil <centraleffects@yahoo.com>
2018-12-10 02:16:42 +0800

    repo init

---
+++ resources/views/dashboard/index.blade.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Subida POS</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
+
+        <!-- Styles -->
+
+    </head>
+    <body>
+        <div class="flex-center position-ref full-height">
+            This is you dashboard
+        </div>
+    </body>
+</html>
