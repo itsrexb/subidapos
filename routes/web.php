6d5b4ce2a6f233be7e0cd7faa33b46d11a116d15
Dexter Bengil <dexterb2992@gmail.com>
2018-12-24 00:09:39 +0800

    Make authentication scaffolding.

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,7 @@ Route::get('/', function () {
 Route::get('/dashboard', function () {
     return view('dashboard/index');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
